28c806e3e4b59b8551a4ea8b1d0b81d55a36b44c
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-07-16 17:29:41 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
+    private static ArrayList <tree> black;
+    private static ArrayList <tree> red;
+    
     public static void main(String[] args){
+        black = new ArrayList();
+        red = new ArrayList();
+        
         Node node = new Node(val, left, right, color);
     }
 }
