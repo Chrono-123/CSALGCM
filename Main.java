893f773392172e4ddf3902e64e3814f3f5b0b8a7
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-07-17 16:30:21 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
 public class Main{
     
     public static void main(String[] args){
-        int root = 6;
-        int left = 5;
-        int right = 7;
-        
         Node node1 = new Node(root, left, right);
-        Node node2 = new node(val, left, right);
-        Node node3 = new node(val, left, right);
-        Node node4 = new node(val, left, right);
-        Node node5 = new node(val, left, right);
-        Node node6 = new node(val, left, right);
-
+        Node node2 = new Node(node1.left, left, right);
+        Node node3 = new Node(node2.left, left, right);
+        Node node4 = new Node(node2.right, left, right);
+        
+        
         
     }
 }
